5a431fda30cb58cb208a4d7c8056c35574247ad7
Andrew Castellano <andrewcastellano@Andrews-MBP.lan>
2017-11-02 23:50:18 -0700

    Added timer that alerts when reaches zero

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,8 +17,8 @@ var questions = [question1, question2, question3, question4, question5, question
 
 var answersOne = ["A. Bail Organa", "B. Lobot","C. Owen Lars", "D. Wedge Antilles"] 
 var answersTwo = ["A. Wilhuff Tarkin", "B. Luke Skywalker", "C. C-3PO", "D. Princess Leia"] 
-var answersThree = ["A. Jawa", "A. 6", "B. Gungan", "C. Dug", "D. Ewok"] 
-var answersFour = [ "A. 6, B. 1", "C. 4", "D. 7"] 
+var answersThree = ["A. Jawa", "B. Gungan", "C. Dug", "D. Ewok"] 
+var answersFour = [ "A. 6", "B. 1", "C. 4", "D. 7"] 
 var answersFive = ["A. Han Solo", "B. Luke Skywalker", "C. Princess Leia", "D. Obi Wan Kenobi"]
 var answersSix = ["A. Dathomir", "B. Corelia", "C. Polis Massa", "D. Rhen Var"] 
 var answersSeven = ["A. Left Arm", "B. Right Leg", "C. Right Arm", "D. Left Leg"] 
@@ -29,10 +29,66 @@ var answersTen = ["A. Darth Vader", "B. Shmi Skywalker", "C. Greedo", "D. Mace W
 var ansArray = [answersOne, answersTwo, answersThree, answersFour, answersFive, answersSix, answersSeven, answersEight, answersNine, answersTen];
 
 var correctAnswer = ["C. Owen Lars", "B. Luke Skywalker", "D. Ewok", "A. 6", "B. Luke Skywalker", "A. Dathomir", "D. Right Arm", "B. Blue", "D. Kamino", "A. Darth Vader"] 
-// var answers {
+// var answers 
 // 	firstQuetion: 
-// 
-var userGuess = []
+
+// this empty array is where all the users guesses will be stored.
+// once all the questions are answered I want to check to see if the values at each index in this array are the same as the correctAnswers array.
+var userGuess = [""]
+
+//  var number = 120;
+
+//     //  Variable that will hold our interval ID when we execute
+//     //  the "run" function
+//     var intervalId;
+
+  
+//     function run() {
+//       intervalId = setInterval(decrement, 1000);
+//     }
+
+//     //  The decrement function.
+//     function decrement() {
+
+//       //  Decrease number by one.
+//       number--;
+//      }var number = 100;
+
+     var number = 100;
+
+    var intervalId;
+
+    
+    function run() {
+    	setTimeout(function(){},5000);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+
+      number--;
+
+      $("#timer").html("<h1>" + number + "<h1>");
+
+      if (number === 0) {
+
+        stop();
+
+        alert("Time Up!");
+      }
+    }
+
+    function stop() {
+
+      clearInterval(intervalId);
+      if(number === 0){
+      	setTimeout(function(){
+		 alert("Time's Up!")},2000);
+      }
+    }
+
+    run();
+
     
 
 function askQuestions(){
@@ -45,9 +101,15 @@ console.log(this);
 
 	console.log(i);
 }
-$("#game-content").click(function(){
+
+// Right here is where I am trying to add the users answer to the userGuess array
+
+$("button").click(function(){
     userGuess.push($(this).val());
 console.log(this);
+console.log(userGuess);
+// I got it to console.log the value of the button that is clicked but that value does not push to the userGuess array.
+// instead the array just adds another space
 })
 
 
